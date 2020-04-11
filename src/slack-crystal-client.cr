daf0fb626818b4f9f09dba7e85cc38a8c72678ea
Marcelo Risoli <celorisoli@gmail.com>
2020-04-11 18:41:41 +0200

    Revert "Add Slack RTM Client Connect"
    This reverts commit 655251e4940bdc81381701cbc8badcac61ee4560.

--- src/slack-crystal-client.cr
+++ src/slack-crystal-client.cr
@@ -1,2 +1,6 @@
-require "json"
-require "./slack/rtm/**"
+# TODO: Write documentation for `Slack::Crystal::Client`
+module Slack::Crystal::Client
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
